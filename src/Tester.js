c87e278f852bc55e11d71463a2262d2784ebb5d4
Justin S. Houk <jsh.dev@gmail.com>
2019-07-07 17:58:50 -0400

    Made Tester content dynamic
    Made what the Tester printed dynamic and now it pulls content from the React Native test server for loading data to a client.

--- src/Tester.js
+++ src/Tester.js
@@ -1,27 +1,35 @@
 import React from "react";
 import axios from "axios";
-import { View, Text } from "react-native";
+import { AppRegistry, View, Text } from "react-native";
+import { name as appName } from "../app.json";
 
 export default class Tester extends React.Component {
-  loadDataFromServer() {
+  constructor(props) {
+    super(props);
+    this.state = { content: "Click to call server" };
+  }
+
+  loadDataFromServer = () => {
+    this.setState({ content: "Calling server..." });
     // Pull data from the web and inject it into the panel
     axios
       .get("https://facebook.github.io/react-native/movies.json")
-      .then(response => response.json())
-      .then(responseJson => {
-        console.log(responseJson.movies);
+      .then(response => {
+        this.setState({ content: JSON.stringify(response.data) });
       })
       .catch(function(error) {
         // handle error
         console.log(error);
       });
-  }
+  };
 
   render() {
     return (
       <View>
-        <Text>This is the tester</Text>
+        <Text onPress={this.loadDataFromServer}>{this.state.content}</Text>
       </View>
     );
   }
 }
+
+AppRegistry.registerComponent(appName, () => Tester);
